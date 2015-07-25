cc5f2f5479eddaa0d308f998057b62005b6968e3
oleh.kastornov <powerbot15@gmail.com>
2015-07-25 21:14:18 +0300

    CRUD create, delete

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,15 +1,38 @@
 Template.messages.helpers({
     messages : function(){
-        //return [{text : 'sdfsdf'} ,{ text : 'sdfsdf'}];
-        return Messages.find()
+        return Messages.find().fetch()
     }
 });
 
 Template.messages.events({
-    'click a' : function(e){
+    'click #add' : addMessage
+});
+
+Template.message.events({
+    'click [data-remove-item]' : function(e){
         e.preventDefault();
-        Messages.insert({
-            text : Template.instance().$('[data-text]').val()
-        })
+        Messages.remove(this._id);
     }
 });
+
+function addMessage(e){
+    e.preventDefault();
+    var template = Template.instance(),
+        messageText = template.$('[data-text]').val(),
+        alreadyExists;
+    if(messageText.trim().length == 0){
+        noty({text : 'Input message first', type : 'error', timeout : 2000});
+        return;
+    }
+    template.$('input').val('').focus();
+    alreadyExists = Messages.find({text : messageText}).fetch();
+    if(alreadyExists.length > 0){
+        noty({text : 'Already exists', type : 'warning', timeout : 2000});
+        return;
+    }
+    Messages.insert({
+        text : messageText
+    })
+
+
+}
